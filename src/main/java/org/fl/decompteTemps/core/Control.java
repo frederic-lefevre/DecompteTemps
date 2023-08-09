678cc560958b6dac292758d187ecd8a2a7c3fb9d
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2023-08-09 08:48:24 +0200

    Refactor Control

--- src/main/java/org/fl/decompteTemps/core/Control.java
+++ src/main/java/org/fl/decompteTemps/core/Control.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+
+Copyright (c) 2017, 2023 Frederic Lefevre
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 package org.fl.decompteTemps.core;
 
 import java.nio.file.Path;
@@ -76,19 +100,31 @@ public final class Control {
      * @return Returns the presenceDirectoryName.
      */
     public static Path getPresenceDirectoryName() {
+        if (! initialized) {
+        	forceInit() ;
+        }
         return presenceDirectoryName;
     }
     
 	
     public static GroupEntity getCurrentGroup() {
+        if (! initialized) {
+        	forceInit() ;
+        }
 		return currentGroup;
 	}
 	
     public static void setCurrentGroup(GroupEntity gr) {
+        if (! initialized) {
+        	forceInit() ;
+        }
 		currentGroup = gr;
 	}
     
     public static GroupEntity getCompleteGroup() {
+        if (! initialized) {
+        	forceInit() ;
+        }
 		return completeGroup;
 	}
     
@@ -99,7 +135,7 @@ public final class Control {
     public static GroupEntity getIndividualEntityAsGroups(String entityName) {
         
         Entity e ;
-        Entity[] entities = completeGroup.getEntities();
+        Entity[] entities = getCompleteGroup().getEntities();
         for (int i=0; i < entities.length; i++) {
             e = (Entity)entities[i] ;
             if (e.getName().equals(entityName)) {
@@ -112,6 +148,9 @@ public final class Control {
     }
 
 	public static Date getEndDate() {
+        if (! initialized) {
+        	forceInit() ;
+        }
 		if (endDateIsNow) {
 			endDate = new Date() ;
 		}
